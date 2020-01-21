fcee1203afcc83094cb910905cba0fcee00aa2ff
gonzalochief <57230398+gonzalochief@users.noreply.github.com>
2020-01-21 13:55:19 -0600

    Update data.go

--- Chapter_2_Go_ChitChat/chitchat/data/data.go
+++ Chapter_2_Go_ChitChat/chitchat/data/data.go
@@ -13,7 +13,7 @@ var Db *sql.DB
 
 func init() {
 	var err error
-	Db, err = sql.Open("postgres", "dbname=chitchat sslmode=disable")
+	Db, err = sql.Open("postgres", "host=127.0.0.1 port=5433 user=postgres password=SDIB2020 dbname=chitchat sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
